d9796a361319aa5b7371eb5c58d089e07ad50a4d
Chris <ChristianFigueroaMontano@outlook.com>
2021-04-27 08:16:57 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+require("dotenv").config()             
+const express = require('express');    
+const path = require('path');
+const bodyParser=require('body-parser')
+const cors=require('cors')
+const hbs = require('express-handlebars');
+const lessMiddleware = require('less-middleware');
+
+
+http.listen(3000, () => console.log('listening on port 3000'));
